5a73b29f72ae4182e5b4f7ebbe8dc3c5c925eb41
Ibrahem-Khaled <xx.vvv48@gmail.com>
2024-06-13 10:58:55 +0200

    first commit

---
+++ database/migrations/2024_06_13_084322_create_rooms_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->enum('status', ['active', 'inactive'])->default('active');
+            $table->unsignedBigInteger('user_id');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('slug');
+            $table->string('image')->nullable();
+            $table->string('password')->nullable();
+            $table->unsignedBigInteger('category_id');
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rooms');
+    }
+};
